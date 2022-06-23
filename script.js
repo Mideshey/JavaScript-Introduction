8a1e2a6d231c9889a71c927d339b286f46a13d6a
Mideshey <101341644+Mideshey@users.noreply.github.com>
2022-06-23 02:50:33 +0100

    printing out my information

---
+++ script.js
@@ -0,0 +1,9 @@
+const aboutme = {
+    name: 'Olukoya Oluwaseyi',
+    height: '5.8 inches',
+    country: 'Nigeria',
+};
+
+alert(
+    `My name is ${aboutme.name} , I am ${aboutme.height} tall and I am from ${aboutme.country}.`
+);
